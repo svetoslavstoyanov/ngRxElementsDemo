d996c14670c5458ef940ae60177876eb9ec3aaf1
Svetoslav Stoyanov <svetoslav39@gmail.com>
2019-10-17 23:11:35 +0300

    implement on keyup / focus on input

--- src/app/todo-add/todo-add.component.ts
+++ src/app/todo-add/todo-add.component.ts
@@ -1,42 +1,45 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, HostListener, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { IAppState } from './../app.state';
-import { ITodo } from './../models/todo';
 import * as TodoActions from './../+store/actions/todo.actions';
 import * as CounterActions from './../+store/actions/counter.actions';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
-import { Observable, Subscription } from 'rxjs';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-todo-add',
   templateUrl: './todo-add.component.html',
   styleUrls: ['./todo-add.component.scss']
 })
-export class TodoAddComponent implements OnInit, OnDestroy {
+export class TodoAddComponent implements OnInit, OnDestroy, AfterViewInit {
+  @ViewChild('searchInput', { static: true }) firstNameElement: ElementRef;
 
   counter = 1;
   subscription: Subscription;
-
-  constructor(private store: Store<IAppState>, private formBuilder: FormBuilder) {
-
-  }
-
   addTodoForm: FormGroup = this.formBuilder.group({
     todo: ['', Validators.required]
   });
 
-  addTodo() {
-    const todo = { todo: this.addTodoForm.value.todo };
-    this.store.dispatch(new TodoActions.AddTodo(todo));
-    this.store.dispatch(new CounterActions.CounterIncrease());
-    this.addTodoForm.reset();
+  constructor(private store: Store<IAppState>, private formBuilder: FormBuilder) {
   }
 
   ngOnInit() {
     this.subscription = this.store.select('counter')
       .subscribe((data) => this.counter = data);
   }
+
+  ngAfterViewInit() {
+    window.addEventListener('keyup', (e) => e.key === '/' && this.firstNameElement.nativeElement.focus());
+  }
+
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
+  }
+
+  addTodo() {
+    const todo = { todo: this.addTodoForm.value.todo };
+    this.store.dispatch(new TodoActions.AddTodo(todo));
+    this.store.dispatch(new CounterActions.CounterIncrease());
+    this.addTodoForm.reset();
   }
 }
